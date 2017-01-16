5c8a32ba78a838910d4c3a47bb82d84bd4a90e1b
Vinícius Marcili <vinipetter@gmail.com>
2017-01-16 14:17:38 -0200

    Termino da aula de pilhas

--- pilha.py
+++ pilha.py
@@ -2,13 +2,16 @@ class stack:
 
 	def __init__(self): 
 		self.stack = []
+		self.len_stack = 0
 
 	def push(self, e):
 		self.stack.append(e)
+		self.len_stack += 1
 
 	def pop(self):
 		if not self.empty():
-			self.stack.pop(len(self.stack) - 1)
+			self.stack.pop(self.len_stack - 1)
+			self.len_stack -= 1
 		else:
 			print('Stack Empty')	
 
@@ -18,18 +21,19 @@ class stack:
 		return 'Stack is Empty'	
 
 	def empty(self):
-		if(len(self.stack) == 0):
+		if self.len_stack == 0:
 			return 'Stack is Empty'
+		return False	
 
 	def length(self):
-		return len(self.stack)	
+		return self.len_stack	
 
 s = stack()
 print(s.empty())
 s.push(1)
 s.push(2)
 s.push(3)
-
+print(s.empty())
 s.pop()
 s.pop()
 print(s.length())
